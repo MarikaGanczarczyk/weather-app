a6c8a72b028570b2b75342b0311ed96a60f7f4d1
Marika <marikabartman27@gmail.com>
2025-10-11 12:04:57 +0100

    Added humidity and wind speed + icons

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import './Weather.css'
-import search_icon from '../assets/search1.png'
+import React from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/sun.png";
+import humidity_icon from "../assets/humidity.png";
+import wind_icon from "../assets/wind.png";
+
 
 function Weather() {
   return (
-    <div>
-      <div className="weather">
-        <div className="search-bar">
-            <input type="text" placeholder='Search' />
-            <img  className='search-icon' src={search_icon} alt="" />
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img className="search-icon" src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">16°c</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" className="weather-icon-2" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" className="weather-icon-2 " />
+          <div>
+            <p>3.6 km/h</p>
+            <span>Wind Speed</span>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Weather
+export default Weather;
